c4753fc14700a07bea4e195c8113a1c2e2a53182
Sampras Lopes <sampras.lopes@haptik.ai>
2020-08-10 16:24:13 +0530

    Update README

--- hsl_builder/Elements/Actionable.py
+++ hsl_builder/Elements/Actionable.py
@@ -51,7 +51,7 @@ class Actionable(object):
         if True, on click of the element, this actionable will get triggered.
 
     payload : dict 
-        optional payload containind metadata that might be needed by the Actionable
+        optional payload containing metadata that might be needed by the Actionable
     """
 
     def __init__(self, text: str, type: ActionableType, uri: URI):
